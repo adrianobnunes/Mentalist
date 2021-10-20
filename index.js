e5aec80d65f6ca2f4b7c54909d573367bb8e3944
Adriano Becco Nunes <76850033+adrianobnunes@users.noreply.github.com>
2021-10-20 11:04:43 -0300

    criação do jogo e estilização

---
+++ index.js
@@ -0,0 +1,22 @@
+let numeroSecreto = parseInt(Math.random() * 10);
+let tentativasUsuario = 3;
+
+while(tentativasUsuario > 0){
+    let chute = parseInt(prompt("Digite um número entre 0 e 10"))
+
+    if(chute === numeroSecreto){
+        alert("PARABÉNS! Você acertou o número secreto!");
+        break
+    } else if(chute > numeroSecreto){
+        alert("O número secreto é menor")
+        tentativasUsuario = tentativasUsuario - 1;
+    } else if(chute < numeroSecreto){
+        alert("O número secreto é maior");
+        tentativasUsuario = tentativasUsuario - 1;
+    } 
+
+    if(chute !== numeroSecreto && tentativasUsuario === 0){
+       alert("FIM DE JOGO! O número secreto era " + numeroSecreto)
+    }
+}
+
